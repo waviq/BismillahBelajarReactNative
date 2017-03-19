91cfe3bfd07a67c039646f3cf704f08765864575
waviq <waviq.subkhi@gmail.com>
2017-03-19 21:07:48 +0700

    network

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,18 +3,29 @@ import {Text, View} from 'react-native'
 import axios from 'axios'
 
 class AlbumList extends React.Component {
+
+    state = {
+        albums:[]
+    };
+
     componentWillMount(){
         axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => {
-                console.log(response)
-            })
+            .then(response => this.setState({albums: response.data}))
+
+            /*.then(function (response) {
+                this.setState({almbums: response.data})
+            })*/
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(isi_album => <Text>{isi_album.title}</Text>)
     }
     render() {
+
+        console.log('hasil :'+this.state.albums);
         return (
             <View>
-                <Text>
-                    Album List
-                </Text>
+                {this.renderAlbums()}
             </View>
         );
     }
