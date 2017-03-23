b3ed3d5d78d79529d2c368596f848fa6ac95cc27
waviq <waviq.subkhi@gmail.com>
2017-03-23 07:31:06 +0700

    styling mastering

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,23 +3,65 @@
  */
 
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection'
+import Button from './Button'
 
 class AlbumDetail extends React.Component {
     render() {
+        const {thumnailStyle,
+            headerContentStyle,
+            thumnailContainerStyle,
+            headerTextStyle,
+            imagesStyle} = styles
+        const {thumbnail_image} = this.props.albumProp
         return (
             <Card>
                 <CardSection>
-                    <Text>
-                        {this.props.albumProp.title}
-                    </Text>
+                    <View style={thumnailContainerStyle}>
+                        <Image style={thumnailStyle} source={{uri: thumbnail_image}}/>
+                    </View>
+                    <View style={headerContentStyle}>
+                        <Text style={headerTextStyle}>{this.props.albumProp.title}</Text>
+                        <Text style={headerTextStyle}>{this.props.albumProp.artist}</Text>
+                    </View>
+                </CardSection>
+                <CardSection>
+                    <Image style={imagesStyle} source={{uri: this.props.albumProp.image}}/>
+                </CardSection>
+                <CardSection>
+                    <Button />
                 </CardSection>
             </Card>
         );
     }
 }
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',//biar tipenya jd tipe kolom k bawah
+        justifyContent: 'space-around' //biar rata kanan n kiri
+    },
+    headerTextStyle:{
+        fontSize: 18
+    },
+    thumnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imagesStyle: {
+        height:300,
+        flex: 1,
+        width:null
+    }
+};
 
 
 export default AlbumDetail
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View} from 'react-native'
+import {View, ScrollView} from 'react-native'
 import axios from 'axios'
 import AlbumDetail from './AlbumDetail'
 
@@ -25,9 +25,9 @@ class AlbumList extends React.Component {
     }
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 
